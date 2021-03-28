1bbaf3c2b7e7d8524998ff687fd8b4ce40d18634
zikwall <andrey.kapitonov.96@gmail.com>
2021-03-28 20:59:16 +0300

    Native broadcasting with RTMP streamer

--- lib/screens/stream/screen.dart
+++ lib/screens/stream/screen.dart
@@ -5,8 +5,10 @@ import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 
 // dependencies
-import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
-import 'package:camera/camera.dart';
+import 'package:rtmp_publisher/camera.dart';
+
+// application
+import 'package:fstreamer/constants/rtmp.dev.dart';
 
 class StreamScreen extends StatefulWidget {
   StreamScreen({Key key}) : super(key: key);
@@ -16,12 +18,13 @@ class StreamScreen extends StatefulWidget {
 }
 
 class _StreamScreenState extends State<StreamScreen> with WidgetsBindingObserver {
-  FlutterFFmpeg _flutterFFmpeg = new FlutterFFmpeg();
-  CameraController _controller;
-  Future<void> _initializeControllerFuture;
   bool mounted = false;
-  bool ffmpgeRunned = false;
-  int ffmpegProcessId = 0;
+  bool enableAudio = true;
+  bool useOpenGL = true;
+  String url = RTMPPush;
+
+  Future<void> _initializeControllerFuture;
+  CameraController _controller;
 
   @override
   void initState() {
@@ -35,7 +38,6 @@ class _StreamScreenState extends State<StreamScreen> with WidgetsBindingObserver
   void dispose() {
     WidgetsBinding.instance?.removeObserver(this);
     _controller?.dispose();
-    _flutterFFmpeg?.cancel();
 
     super.dispose();
   }
@@ -44,18 +46,46 @@ class _StreamScreenState extends State<StreamScreen> with WidgetsBindingObserver
   void didChangeAppLifecycleState(AppLifecycleState state) {
     print("CHANGE LIFE CYCLE $state");
 
-    if (state == AppLifecycleState.resumed) {
-      _controller != null
-          ? _initializeControllerFuture = _controller.initialize()
-          : null; //on pause camera is disposed, so we need to call again "issue is only for android"
-    } else if (state == AppLifecycleState.paused) {
-      _controller != null
-          ? _initializeControllerFuture = _controller.initialize()
-          : null;
-    } else if (state == AppLifecycleState.inactive) {
-      _controller != null
-          ? _initializeControllerFuture = _controller.initialize()
-          : null;
+    if (_controller == null || !_controller.value.isInitialized) {
+      return;
+    }
+
+    if (state == AppLifecycleState.inactive) {
+      _controller?.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      if (_controller != null) {
+        onNewCameraSelected(_controller.description);
+      }
+    }
+  }
+
+  void onNewCameraSelected(CameraDescription cameraDescription) async {
+    if (_controller != null) {
+      await _controller.dispose();
+    }
+    _controller = CameraController(
+      cameraDescription,
+      ResolutionPreset.medium,
+      enableAudio: enableAudio,
+      androidUseOpenGL: useOpenGL,
+    );
+
+    // If the controller is updated then update the UI.
+    _controller.addListener(() {
+      if (mounted) setState(() {});
+      if (_controller.value.hasError) {
+        print('Camera error ${_controller.value.errorDescription}');
+      }
+    });
+
+    try {
+      await _controller.initialize();
+    } on CameraException catch (e) {
+      print(e);
+    }
+
+    if (mounted) {
+      setState(() {});
     }
   }
 
@@ -80,25 +110,94 @@ class _StreamScreenState extends State<StreamScreen> with WidgetsBindingObserver
     });
   }
 
-  runFfmpeg() async {
-    var arguments = [
-      "-y", "-video_size", "hd720",
-      "-f", "android_camera",
-      "-i", "0:0",
-      "-s", "720x480",
-      "-c:a", "copy", "-c:v", "copy",
-      "-vcodec", "flv1",
-      "-f", "flv",
-      "rtmp://vp-push-ix1.gvideo.co/in/53304?bf5f5142a6b37e8962fa75d3f20d74e5"
-    ];
+  Future<String> startVideoStreaming() async {
+    if (!_controller.value.isInitialized) {
+      print('Error: select a camera first.');
+      return null;
+    }
 
-    int pid = await _flutterFFmpeg.executeAsyncWithArguments(arguments, (rc) {
-      print("FFmpeg process exited with rc $rc");
+    if (_controller.value.isStreamingVideoRtmp) {
+      print('Error: already steamed.');
+      return null;
+    }
+
+    String _url = url;
+
+    try {
+      await _controller.startVideoStreaming(_url);
+    } on CameraException catch (e) {
+      print(e);
+      return null;
+    }
+
+    return _url;
+  }
+
+  Future<void> stopVideoStreaming() async {
+    if (!_controller.value.isStreamingVideoRtmp) {
+      print('Error: camera not streamed.');
+      return null;
+    }
+
+    try {
+      await _controller.stopVideoStreaming();
+    } on CameraException catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  Future<void> pauseVideoStreaming() async {
+    if (!_controller.value.isStreamingVideoRtmp) {
+      print('Error: camera not streamed.');
+      return null;
+    }
+
+    try {
+      await _controller.pauseVideoStreaming();
+    } on CameraException catch (e) {
+      print(e);
+      rethrow;
+    }
+  }
+
+  Future<void> resumeVideoStreaming() async {
+    if (!_controller.value.isStreamingVideoRtmp) {
+      print('Error: camera not streamed.');
+      return null;
+    }
+
+    try {
+      await _controller.resumeVideoStreaming();
+    } on CameraException catch (e) {
+      print(e);
+      rethrow;
+    }
+  }
+
+  void onVideoStreamingButtonPressed() {
+    startVideoStreaming().then((String url) {
+      if (mounted) setState(() {});
+
+      if (url != null) {
+        print('Streaming video to $url');
+      }
     });
+  }
 
-    setState(() {
-      ffmpgeRunned = true;
-      ffmpegProcessId = pid;
+  void onStopButtonPressed() {
+    if (this._controller.value.isStreamingVideoRtmp) {
+      stopVideoStreaming().then((_) {
+        if (mounted) setState(() {});
+        print('Video streaming stopped');
+      });
+    }
+  }
+
+  void onPauseStreamingButtonPressed() {
+    pauseVideoStreaming().then((_) {
+      if (mounted) setState(() {});
+      print('Video streaming paused');
     });
   }
 
@@ -146,26 +245,11 @@ class _StreamScreenState extends State<StreamScreen> with WidgetsBindingObserver
             ),
             floatingActionButton: FloatingActionButton(
               child: Icon(Icons.camera_alt),
-              // Provide an onPressed callback.
               onPressed: () async {
-                // Take the Picture in a try / catch block. If anything goes wrong,
-                // catch the error.
-                try {
-                  // Ensure that the camera is initialized.
-                  if (ffmpgeRunned) {
-                    _flutterFFmpeg.cancelExecution(ffmpegProcessId);
-
-                    setState(() {
-                      ffmpegProcessId = 0;
-                      ffmpgeRunned = false;
-                    });
-                  } else {
-                    //await _initializeControllerFuture;
-                    runFfmpeg();
-                  }
-                } catch (e) {
-                  // If an error occurs, log the error to the console.
-                  print(e);
+                if (!_controller.value.isStreamingVideoRtmp) {
+                  onVideoStreamingButtonPressed();
+                } else {
+                  onStopButtonPressed();
                 }
               },
             ),
